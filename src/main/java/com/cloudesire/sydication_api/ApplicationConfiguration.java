5371fb11133330c9d6f5e1f4e9323cf44cafc1b8
Giovanni Toraldo <g.toraldo@liberologico.com>
2015-07-22 14:23:14 +0200

    Minor fix: indentation

--- src/main/java/com/cloudesire/sydication_api/ApplicationConfiguration.java
+++ src/main/java/com/cloudesire/sydication_api/ApplicationConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "syndication")
+@ConfigurationProperties ( prefix = "syndication" )
 public class ApplicationConfiguration
 {
     private static final Logger log = LoggerFactory.getLogger( ApplicationConfiguration.class );
